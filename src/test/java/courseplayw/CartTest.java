2d059e59826c379ae73e2f16f3fdc00ca8d47669
savada <nikbon29@gmail.com>
2025-12-09 12:36:03 +0300

    Добавлена логика скриншота при падении

--- src/test/java/courseplayw/CartTest.java
+++ src/test/java/courseplayw/CartTest.java
@@ -1,13 +1,15 @@
 package courseplayw;
 
 import com.microsoft.playwright.Locator;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class CartTest extends BaseTest{
+public class CartTest extends BaseTest {
+    private static boolean testPassed = true;
 
     @Test
-    void testAddRemoveElements(){
+    void testAddRemoveElements() {
         page.navigate("https://the-internet.herokuapp.com/add_remove_elements/");
         page.click("button:has-text('Add Element')");
         Locator deleteButton = page.locator("button.added-manually");
@@ -18,5 +20,13 @@ public class CartTest extends BaseTest{
 
         Assertions.assertEquals(0, page.locator("button.added-manually").count());
         captureScreenshot("deleted element");
+    }
+
+    @AfterEach
+    void saveScreenshotIfFailed() {
+        if (!testPassed) {
+            captureScreenshot("test_failed");
+            System.out.println("✅ Скриншот при падении сохранен");
+        }
     }
 }
