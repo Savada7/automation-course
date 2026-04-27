cc95288c3115cfc59ef290cc3d61fa0c2eaf3c7a
Nikita_Bondarev <Traue7@yandex.com>
2026-04-28 00:40:37 +0300

    Практическое задание 25: Проверить данные в БД с использованием PostgreSQL

---
+++ src/main/java/com/qa/config/DbConfig.java
@@ -0,0 +1,15 @@
+package com.qa.config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources("classpath:dbconfig.properties")
+public interface DbConfig extends Config {
+    @Key("db.url")
+    String dbUrl();
+
+    @Key("db.user")
+    String dbUser();
+
+    @Key("db.password")
+    String dbPassword();
+}
